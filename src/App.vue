16f22787a17ff9892ac58c599b143bc1908b7ee4
DenisConsta <denisconstantin338@gmail.com>
2022-11-14 16:27:49 +0200

    [init]

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <AppTitle></AppTitle>
+  <div class="container">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur eaque corrupti accusantium distinctio molestiae obcaecati in iste ipsam id iure aliquam quidem, perspiciatis unde. Illum vero blanditiis eius fugit suscipit!</p>
+  </div>
+</template>
+
+<script>
+import AppTitle from "./components/AppTitle.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppTitle,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1> {{title}} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppTitle",
+  data() {
+    return {
+      title: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+  h1{
+    color: blueviolet;
+    font-size: 3rem;
+    font-family: italic;
+  }
+
+</style>
